b27efc8df56587932756171b16baf9bba6684db2
Min Soung Choi <m.choi960@gmail.com>
2019-01-13 15:34:43 -0700

    First commit

---
+++ app/src/main/java/minsoung/koreankeyboard/KoreanKeyboardView.java
@@ -0,0 +1,12 @@
+package minsoung.koreankeyboard;
+
+import android.content.Context;
+import android.inputmethodservice.KeyboardView;
+import android.util.AttributeSet;
+
+public class KoreanKeyboardView extends KeyboardView {
+    public KoreanKeyboardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.m
+    }
+}
